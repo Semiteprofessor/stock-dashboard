dfdaf09d427536a5288a88309228f748d81814bb
Taiwo Olapade <semiteprofessor@gmail.com>
2025-08-22 00:02:33 +0100

    Stock Dashboard

--- src/redux/slices/stocks.js
+++ src/redux/slices/stocks.js
@@ -4,10 +4,6 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   portfolio: {
     holdings: [],
-    investedValue: 0,
-    currentValue: 0,
-    profitLoss: 0,
-    cashBalance: 0,
   },
 };
 
@@ -18,20 +14,7 @@ const slice = createSlice({
     getHoldings: (state, action) => {
       const holdings = action.payload;
 
-      const investedValue = sum(
-        holdings.map((stock) => stock.buyPrice * stock.quantity)
-      );
-
-      const currentValue = sum(
-        holdings.map((stock) => stock.currentPrice * stock.quantity)
-      );
-
-      const profitLoss = currentValue - investedValue;
-
       state.portfolio.holdings = holdings;
-      state.portfolio.investedValue = investedValue;
-      state.portfolio.currentValue = currentValue;
-      state.portfolio.profitLoss = profitLoss;
     },
   },
 });
